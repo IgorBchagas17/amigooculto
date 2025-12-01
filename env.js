b34f8f88907d49a62815847d3b13b080826e2908
Igor Batista <pedracaiu3@gmail.com>
2025-12-01 15:00:30 -0300

    Forcando envio do env.js

---
+++ env.js
@@ -0,0 +1,5 @@
+// env.js
+const SUPABASE_URL = 'https://npyaichslorbcuummmmu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weWFpY2hzbG9yYmN1dW1tbW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MDM4NzIsImV4cCI6MjA4MDE3OTg3Mn0.vHnBbxstQRQikaXmZaaZ3gBt01Rrv3rcAYF1t2TaA_I';
+
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
